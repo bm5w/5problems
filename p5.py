a4253f7b8adea3195f01bba20d67870da8213f90
Mark Saiget <saiget@gmail.com>
2015-05-11 20:53:49 -0700

    Remove current != 9.

--- p5.py
+++ p5.py
@@ -2,7 +2,7 @@ def sum100(current=2, sequence='1'):
     """Recursive method for finding sequences that sum to 100."""
     if current <= 9:
         sum100(current+1, sequence+str(current))
-        if sequence[-1] != u'-' and sequence[-1] != u'+' and current != 9:
+        if sequence[-1] != u'-' and sequence[-1] != u'+':
             sum100(current, sequence+u'+')
             sum100(current, sequence+u'-')
     else:
