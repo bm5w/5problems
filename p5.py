a70b4c753ee84c5d30aaedf2ec32c15a69f177c1
Mark Saiget <saiget@gmail.com>
2015-05-11 20:50:50 -0700

    Clean up code.

--- p5.py
+++ p5.py
@@ -2,7 +2,7 @@ def sum100(current=2, sequence='1'):
     """Recursive method for finding sequences that sum to 100."""
     if current <= 9:
         sum100(current+1, sequence+str(current))
-        if sequence[-1] != u'-' and sequence[-1] != u'+':
+        if sequence[-1] != u'-' and sequence[-1] != u'+' and current != 9:
             sum100(current, sequence+u'+')
             sum100(current, sequence+u'-')
     else:
@@ -27,7 +27,7 @@ def tot(sequence, total=0):
             if sequence[counter] == u'+':
                 plus_minus = 1
         counter += 1
-    total = total + (int(temp)*plus_minus)
+    total = int(temp)*plus_minus
     return total
 
 if __name__ == "__main__":
