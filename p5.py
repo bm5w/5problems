17557ba284f6e0cfade38e7688831a5df5f7fbaa
Mark Saiget <saiget@gmail.com>
2015-05-11 20:07:15 -0700

    Add docstrings.

--- p5.py
+++ p5.py
@@ -1,4 +1,5 @@
 def sum100(current=2, sequence='1'):
+    """Recursive method for finding sequences that sum to 100."""
     if current <= 9:
         sum100(current+1, sequence+str(current))
         if sequence[-1] != u'-' and sequence[-1] != u'+':
@@ -11,12 +12,13 @@ def sum100(current=2, sequence='1'):
 
 
 def tot(sequence, total=0):
-    """plus_minus = 1 or -1"""
+    """Helper method for finding total of string sequence.
+
+    plus_minus = 1 or -1"""
     total = 0
     temp = ''
     counter = 0
     plus_minus = 1
-    # print 'seq: {}'.format(sequence)
     while counter < len(sequence):
         try:
             int(sequence[counter])
