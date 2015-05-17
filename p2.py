1438f82f66e1fa8f259ce19adf96eeab1a7ceb80
Mark Saiget <saiget@gmail.com>
2015-05-16 22:51:37 -0700

    add docstring to p2.

--- p2.py
+++ p2.py
@@ -1,4 +1,5 @@
 def combine(list1, list2):
+    """Write a function that combines two lists by alternatingly taking elements. For example: given the two lists [a, b, c] and [1, 2, 3], the function should return [a, 1, b, 2, c, 3]."""
     output = []
     for i in range(max(len(list1), len(list2))):
         output.append(list1[i])
