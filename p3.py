8deeab0c4da1907c3964ed45640141303ededb49
Mark Saiget <saiget@gmail.com>
2015-06-03 15:02:49 -0700

    Add docstring to p3.py

--- p3.py
+++ p3.py
@@ -1,3 +1,11 @@
+"""
+Write a function that computes the list of the first 100 Fibonacci numbers. By
+definition, the first two numbers in the Fibonacci sequence are 0 and 1, and
+each subsequent number is the sum of the previous two. As an example, here are
+the first 10 Fibonnaci numbers: 0, 1, 1, 2, 3, 5, 8, 13, 21, and 34.
+"""
+
+
 def fib(total=100):
     output = []
     for i in xrange(100):
